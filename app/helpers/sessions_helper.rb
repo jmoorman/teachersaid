49f1d0d799d4c46f25a68110030e5a59a6a0a429
Jon Moorman <moorman.jon@gmail.com>
2013-05-30 19:36:52 -0400

    fixed some typos of User to Teacher. added sign in/out link to all pages

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,19 +1,19 @@
 module SessionsHelper
 
-   def sign_in(user)
-      cookies.permanent[:remember_token] = user.remember_token
-      self.current_user = user
+   def sign_in(teacher)
+      cookies.permanent[:remember_token] = teacher.remember_token
+      self.current_teacher = teacher
    end
 
    def signed_in?
-      !current_user.nil?
+      !current_teacher.nil?
    end
 
-   def current_user=(user)
-      @current_user = user
+   def current_teacher=(teacher)
+      @current_teacher = teacher
    end
 
-   def current_user
-      @current_user ||= User.find_by_remember_token(cookies[:remember_token])
+   def current_teacher
+      @current_teacher ||= Teacher.find_by_remember_token(cookies[:remember_token])
    end
 end
